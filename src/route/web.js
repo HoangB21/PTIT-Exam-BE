da82a8347e31ce3dc9c6711d62ef7ae05b725cd3
hieuthanhpro <hieuthanh1203@gmail.com>
2024-04-03 13:47:13 +0700

    question api

--- src/route/web.js
+++ src/route/web.js
@@ -1,15 +1,21 @@
 import express from "express";
 import userController from "../controllers/userController";
-
+import questionController from "../controllers/questionController"
 let router = express.Router();
 
 let initWebRoutes = (app) => {
 
-    router.post('/api/login', userController.loginUserController);
-    router.get('/api/get-user', userController.getUserController);
-    router.post('/api/create-user', userController.createUserController);
-    router.put('/api/update-user', userController.updateUserController);
-    router.delete('/api/delete-user', userController.deleteUserController);
+    router.post("/api/v1/login", userController.loginUserController);
+    router.get("/api/v1/get-user", userController.getUserController);
+    router.post("/api/v1/create-user", userController.createUserController);
+    router.put("/api/v1/update-user", userController.updateUserController);
+    router.delete("/api/v1/delete-user", userController.deleteUserController);
+    // api question
+    router.get('/api/v1/get-question', questionController.getQuestionController);
+    router.post('/api/v1/create-question', questionController.createQuestionController);
+    router.delete('/api/v1/delete-question',questionController.deleteQuestionController);
+    router.put('/api/v1/update-question',questionController.updateQuestionController);
+
     return app.use("/", router);
 }
 
---
+++ src/services/questionService.js
@@ -0,0 +1,122 @@
+import bcrypt from "bcryptjs";
+import db from '../models/index';
+
+const salt = bcrypt.genSaltSync(10);
+
+let createQuestion = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.content) {
+                resolve({
+                    errorCode: 1,
+                    message: "Missing question's information"
+                });
+                return;
+            }
+            if (await checkContentExist(data.content)) {
+                resolve({
+                    errorCode: 2,
+                    message: "Content already exists"
+                });
+                return;
+            }
+            await db.Question.create({
+                examid: data.examid,
+                content: data.content
+            });
+            resolve({
+                errorCode: 0,
+                message: "Create question successfully!"
+            });
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+let checkContentExist = async (content) => {
+    let data = await db.Question.findOne({ where: { content: content } });
+    if (data) return true;
+    return false;
+}
+
+let getQuestion = (questionid,examid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = (questionid === 'ALL')
+                ? await db.Question.findAll({where:{examid:examid}})
+                : await db.Question.findOne({ where: { questionid:questionid } });
+            if (data && data.length > 0) resolve(data);
+            else resolve(
+                {
+                    msg: "question not found"
+                }
+            );
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+let deleteQuestion = (data) => {
+    return new Promise(async (resolve, reject) => {
+        let id = data.id
+        try {
+            if (!id) {
+                resolve({
+                    errorCode: 1,
+                    message: "Missing some parameters"
+                })
+            }
+            let question = await db.Question.findOne({ where: { questionid: id }, raw: false });
+            if (!question) {
+                resolve({
+                    errorCode: 2,
+                    message: "Question not found"
+                })
+            }
+            await question.destroy();
+            resolve({
+                errorCode: 0,
+                message: "Deleted"
+            });
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+let updateQuestion = (data) => {
+    return new Promise(async (resolve, reject) => {
+        let id = data.id
+        try {
+            if (!id) {
+                resolve({
+                    errorCode: 1,
+                    message: "Missing some parameters"
+                })
+            }
+            let question = await db.Question.findOne({ where: { id: id }, raw: false });
+            if (!question) {
+                resolve({
+                    errorCode: 2,
+                    message: "question not found"
+                })
+            }
+            question.content = data.content;
+            await question.save();
+            resolve({
+                errorCode: 0,
+                message: "Updated"
+            });
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+module.exports = {
+    createQuestion: createQuestion,
+    getQuestion: getQuestion,
+    createQuestion: createQuestion,
+    updateQuestion: updateQuestion,
+    deleteQuestion: deleteQuestion,
+}
